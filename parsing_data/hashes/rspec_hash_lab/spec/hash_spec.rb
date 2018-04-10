d11feba0ee1d9981c9b50a76e2e8d4ed69e3c6a5
Tech-A41 <tech-a41@Tech-A41s-MacBook-Air.local>
2018-04-10 17:12:25 +0100

    last checks complete

--- parsing_data/hashes/rspec_hash_lab/spec/hash_spec.rb
+++ parsing_data/hashes/rspec_hash_lab/spec/hash_spec.rb
@@ -3,34 +3,34 @@ require_relative 'spec_helper'
 describe 'Create tests for a hash' do
 
   small_fruit_nutrition_details = {
-      apple:{
-          calories: 130,
-          calories_from_fat: 0,
-          sugars: '25g',
-          protein: '1g'
+    apple:{
+      calories: 130,
+      calories_from_fat: 0,
+      sugars: '25g',
+      protein: '1g'
 
-      },
-      avocado:{
-          calories: 50,
-          calories_from_fat: 35,
-          sugars: '0g',
-          protein: '1g'
+    },
+    avocado:{
+      calories: 50,
+      calories_from_fat: 35,
+      sugars: '0g',
+      protein: '1g'
 
-      },
-      kiwifruit:{
-          calories: 90,
-          calories_from_fat: 10,
-          sugars: '13g',
-          protein: '1g'
+    },
+    kiwifruit:{
+      calories: 90,
+      calories_from_fat: 10,
+      sugars: '13g',
+      protein: '1g'
 
-      },
-      plums:{
-          calories: 70,
-          calories_from_fat: 0,
-          sugars: '16g',
-          protein: '1g'
+    },
+    plums:{
+      calories: 70,
+      calories_from_fat: 0,
+      sugars: '16g',
+      protein: '1g'
 
-      }
+    }
   }
 
   it 'avocados should have calories from fat great than 20' do
@@ -46,8 +46,8 @@ describe 'Create tests for a hash' do
 
   it 'each fruit should have 4 keys' do
     small_fruit_nutrition_details.each do | k, v|
-    expect(v.keys.length).to be 4
-  end
+      expect(v.keys.length).to eq 4
+    end
 
   end
 
